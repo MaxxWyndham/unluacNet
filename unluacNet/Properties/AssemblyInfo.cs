efb76e0d2d515e884245fedced46aa6813ea3b84
Maxx Wyndham <maxx@marketingradar.com>
2015-11-10 18:01:59 +0000

    Tweaked some assembly info

--- unluacNet/Properties/AssemblyInfo.cs
+++ unluacNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("unluacNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("unluac.net")]
+[assembly: AssemblyDescription("A decompiler for Lua 5.1 in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("unluacNet")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Original by tehtmi, ported by Alex Darby, maintained by Maxx Wyndham")]
+[assembly: AssemblyProduct("unluac.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
